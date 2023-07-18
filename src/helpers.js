4f100c349536dfb53465fd212a0b899ffae27664
akinlope <akinrimisitt@gmail.com>
2023-07-18 17:29:07 +0100

    Done with the links

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,8 @@
 import bueno from "./image/Bueno.png"
-import clock from "./image/Clock.png"
+// import clock from "./image/Clock.png"
 import portfolio from "./image/Portfolio.png"
 import todo from "./image/Todo.png"
+import budget from "./image/budget.png"
 
 export const videoArr = [
     {
@@ -17,8 +18,16 @@ export const videoArr = [
         url: "https://res.cloudinary.com/dkhx7sgt1/video/upload/v1682516038/for%20portfolio/portfolio_ebpvae.mp4",
         disc: "A simple working portfolio website where others van see all I have done so far and contact me.",
         link: "http://www.itolu.netlify.app",
-        visit: "https://github.com/akinlope/mainportfolio",
+        visit: "https://github.com/akinlope/iTolu",
         thumbnail: portfolio
+    },
+    {
+        name: "Budgeting app",
+        disc: "Personal budgeting app that helps you maintain and control your expenses.",
+        link: "https://itolubudget.netlify.app",
+        visit: "https://github.com/akinlope/BudgettingApp",
+        thumbnail: budget
+
     },
     {
         name: "Todo ",
@@ -28,14 +37,14 @@ export const videoArr = [
         visit: "https://github.com/akinlope/react-todo",
         thumbnail: todo
     },
-    {
-        name: "Clock Webapp",
-        url: "https://res.cloudinary.com/dkhx7sgt1/video/upload/v1682516036/for%20portfolio/clock_qcsldq.mp4",
-        disc: "A simple web page that tells the time and date",
-        link: "https://unrivaled-taffy-4ff6d4.netlify.app",
-        visit: "https://github.com/akinlope/firstReact",
-        thumbnail: clock
-    },
+    // {
+    //     name: "Clock Webapp",
+    //     url: "https://res.cloudinary.com/dkhx7sgt1/video/upload/v1682516036/for%20portfolio/clock_qcsldq.mp4",
+    //     disc: "A simple web page that tells the time and date",
+    //     link: "https://unrivaled-taffy-4ff6d4.netlify.app",
+    //     visit: "https://github.com/akinlope/firstReact",
+    //     thumbnail: clock
+    // },
     
 ];
 
